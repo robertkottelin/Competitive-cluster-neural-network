eef663f8c6da4a842a82884b9e74669c5861049c
Robert Kottelin <robert.kottelin@gmail.com>
2023-05-09 12:55:06 +0200

    preprocessing

--- rust/preprocess.py
+++ rust/preprocess.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import re
 
 def process_file(input_file, output_file):
@@ -13,18 +12,20 @@ def process_file(input_file, output_file):
     formatted_vectors = []
     for vector in fam_vectors:
         # Use regex to find all numbers in the string
-        numbers = re.findall(r"[-+]?\d*\.\d+|\d+", vector)
+        numbers = re.findall(r"[-+]?\d*\.\d+e[-+]?\d+", vector)
 
         # Convert to floats and format as Rust array
         float_numbers = [float(num) for num in numbers]
-        formatted_vector = f"{float_numbers},"
+        formatted_vector = " ".join(f"{num}" for num in float_numbers)
 
         formatted_vectors.append(formatted_vector)
 
+    # Remove empty lines
+    formatted_vectors = [vector for vector in formatted_vectors if vector]
+
     # Write to output file
     with open(output_file, "w") as f:
-        for vector in formatted_vectors:
-            f.write(vector + "\n")
+        f.write("\n".join(formatted_vectors))
 
 if __name__ == "__main__":
     process_file("vectors.txt", "formatted_vectors.txt")
